37474806b2de7220707d1c456a490609545be5fa
nitinverma9 <40539300+nitinverma9@users.noreply.github.com>
2022-06-12 17:03:40 +0530

    tidying up a bit

--- tf-module-elasticache/main.tf
+++ tf-module-elasticache/main.tf
@@ -1,23 +1,59 @@
 resource "aws_elasticache_cluster" "main" {
-  count                = var.elasticache_node_type = 'memcached' ? 1 : 0
-  cluster_id           = var.cluster_id_or_name
-  engine               = var.elasticache_engine
-  node_type            = var.elasticache_node_type
-  num_cache_nodes      = var.number_of_nodes
+  cluster_id           = var.cluster_id
+  engine               = var.engine
+  engine_version       = var.engine_version
+  node_type            = var.node_type
+  num_cache_nodes      = var.num_cache_nodes
   parameter_group_name = aws_elasticache_parameter_group.main.id
   port                 = 11211
+  az_mode              = var.az_mode
+  maintenance_window   = var.maintenance_window
+  security_group_ids   = [aws_security_group.main.id]
+  subnet_group_name    = aws_elasticache_subnet_group.main.name
+  tags = merge({
+    managedby = "terraform"
+  }, var.extra_tags)
 }
 
 resource "aws_elasticache_parameter_group" "main" {
-  count = var.elasticache_node_type = 'memcached' ? 1 : 0
   name   = "memcached-parameter-group"
   family = var.parameter_group_family
 
- dynamic "parameter" {
-   for_each = length(var.custome_parameter_group_values) > 0 ? var.custome_parameter_group_values : []
-   content {
-     name  = parameter.value.name
-     value = parameter.value.value
-   }
+  dynamic "parameter" {
+    for_each = length(var.custom_parameter_group_values) > 0 ? var.custom_parameter_group_values : []
+    content {
+      name  = parameter.value.name
+      value = parameter.value.value
+    }
   }
 }
+
+resource "aws_elasticache_subnet_group" "main" {
+  name       = var.subnet_group_name
+  subnet_ids = var.vpc_subnet_ids
+}
+
+resource "aws_security_group" "main" {
+  name = "memcached_security_group"
+  ingress {
+    from_port   = 11211
+    to_port     = 11211
+    protocol    = "tcp"
+    cidr_blocks = var.cidr_blocks
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  tags = {
+    "Name" = "memcached_security_group"
+  }
+}
+
+# resource "aws_elasticache_security_group" "bar" {
+#   name                 = "elasticache-security-group"
+#   security_group_names = [aws_security_group.main.name]
+# }
--- tf-module-elasticache/variables.tf
+++ tf-module-elasticache/variables.tf
@@ -1,31 +1,66 @@
-variable "cluster_id_or_name" {
+variable "cluster_id" {
   default = "default-memcached-cluster"
 }
 
-variable "elasticache_engine" {
+variable "engine" {
   default = "memcached"
   type    = string
 }
 
-variable "cluster_id_or_name" {
-  default = "default-memcached-cluster"
+variable "engine_version" {
+  default = "1.6.12"
+  type    = string
 }
-
-variable "elasticache_node_type" {
-  default = ""
+variable "node_type" {
+  default = "cache.t2.micro"
   type    = string
 }
 
-variable "number_of_nodes" {
-  default = 3
-  type    = number
-}
-
-variable "cluster_id_or_name" {
-  default = "default-memcached-cluster"
+variable "num_cache_nodes" {
+  default     = 3
+  type        = number
+  description = "Number of elasticache memcache nodes"
 }
 
 variable "parameter_group_family" {
-  default = "memcached"
+  default = "memcached1.6"
   type    = string
+}
+
+variable "subnet_group_name" {
+  default = "default-memcached-cluster-subnet-group"
+  type    = string
+}
+
+variable "custom_parameter_group_values" {
+  default = []
+}
+
+variable "subnet_group" {
+  default = "memcached-default-subnet-group"
+  type    = string
+}
+
+variable "vpc_subnet_ids" {
+  default = []
+  type    = list(string)
+}
+
+variable "cidr_blocks" {
+  default = ["0.0.0.0/0"]
+  type    = list(string)
+}
+
+variable "az_mode" {
+  default = "single-az"
+  type    = string
+}
+
+variable "maintenance_window" {
+  default = "sun:00:00-sun:01:00"
+  type    = string
+}
+
+variable "extra_tags" {
+  default = {}
 }
