864b6eb8392d804319853501d025d77f43205b6b
nitinverma9 <40539300+nitinverma9@users.noreply.github.com>
2022-06-12 17:05:51 +0530

    tidying up a bit more

--- tf-module-elasticache/memcached/main.tf
+++ tf-module-elasticache/memcached/main.tf
@@ -52,8 +52,3 @@ resource "aws_security_group" "main" {
     "Name" = "memcached_security_group"
   }
 }
-
-# resource "aws_elasticache_security_group" "bar" {
-#   name                 = "elasticache-security-group"
-#   security_group_names = [aws_security_group.main.name]
-# }
